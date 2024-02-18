1adff04d8e7590deec29055038624f7a02d4fc7d
Ashikparvez89 <ashikparvez89@gmail.com>
2024-02-18 20:29:53 +0800

    javascript added

--- js/utitlity.js
+++ js/utitlity.js
@@ -1,34 +1,36 @@
 const seats = document.querySelectorAll('.seats');
 
+let counterr = 0;
+
+let decreaser = 40;
+
+let totalPrice = 0;
+
+let discountprice = 0;
+
 for (let index = 0; index < seats.length; index++) {
     const seat = seats[index]
     seat.addEventListener('click', myfunction);
     function myfunction() {
-        console.log('seat is click')
+
         seat.classList.add('bg-[#1dd100]');
         seat.classList.add('text-white');
         seat.classList.remove('bg-[#e4e7e7]');
+        const seatTable = document.getElementById('seat-calculate');
 
+        // const prize = document.getElementById('price');
+        // const prices = parseFloat(prize.innerText);
 
+        const getvaluetext = seat.querySelector('h3'); // Assuming you want the first <h3> element inside 'seats'
+        const getinnertext = getvaluetext.innerText;
+        console.log(getinnertext);
 
-        const seatTable = document.getElementById('seat-container');
+        // const sea = document.getElementById('c2');
 
-
-
-        const prize = document.getElementById('price');
-        const prices = parseFloat(prize.innerText);
-        console.log(prices);
-
-
-
-  
-        const sea = document.getElementById('c2');
-        const newseat = document.createElement('h1');
-        const seatname = sea.innerText;
-        newseat.innerText = seatname;
+        const newseat = document.createElement('h3');
+        newseat.innerText = getinnertext;
         newseat.classList.add('text-[#65686e]', 'text-xl', 'font-semibold');
         seatTable.appendChild(newseat);
-
 
         const classes = document.getElementById('chooseClass');
         const newClass = document.createElement('h2');
@@ -37,14 +39,79 @@ for (let index = 0; index < seats.length; index++) {
         newClass.classList.add('text-[#65686e]', 'text-xl', 'font-semibold');
         seatTable.appendChild(newClass);
 
-        const pricess = document.getElementById('cash');
-        const newprices = document.createElement('span');
+        const pricess = document.getElementById('price');
         const pricesset = pricess.innerText;
-        newprices.innerText = pricesset;
+        const cash = parseFloat(pricesset);
+        const newprices = document.createElement('span');
+        newprices.innerText = cash;
         newprices.classList.add('text-[#65686e]', 'text-xl', 'font-semibold');
         seatTable.appendChild(newprices);
+
+
+        // calculate total price
+        totalPrice += cash;
+        console.log(totalPrice)
+        const getPrice = document.getElementById('priceCalculate');
+        const priceTonumber = parseFloat(getPrice.innerText);
+        getPrice.innerText = totalPrice;
+
+        // calculate discount-price
+
+        discountprice += cash;
+        console.log(totalPrice)
+        const getdiscountPrice = document.getElementById('Discount');
+        const discountpriceTonumber = parseFloat(getdiscountPrice.innerText);
+        getdiscountPrice.innerText = discountprice;
+
+
+        // seat counter
+
+        const counts = document.getElementById('counter');
+        let count = parseFloat(counts.innerText);
+        count = counterr + 1;
+        counterr++;
+        console.log(count);
+        counts.innerText = count;
+
+        // seat balance  
+
+        const balanceSeat = document.getElementById('balance-seat');
+        let balances = parseFloat(balanceSeat.innerText);
+        balances = decreaser - 1;
+        console.log(balances)
+        decreaser--;
+        balanceSeat.innerText = balances;
+
     }
 }
 
-console.log('why this is')
+
+// discount-section
+
+
+const discountBtn = document.getElementById('apply-btn');
+
+discountBtn.addEventListener('click', function () {
+    console.log('clicked')
+    const getcoupon = document.getElementById('coupon-code').value;
+    const coupon = getcoupon.toUpperCase();
+    console.log(coupon);
+    const couponSection = document.getElementById('coupun-section');
+    console.log(couponSection);
+    if (coupon === 'new20' || coupon === 'couple20') {
+        discountBtn.removeAttribute('disabled')
+    }
+    if (coupon === 'NEW20') {
+        console.log('you get 20 %');
+        couponSection.classList.add('hidden')
+
+    }
+    else if (coupon === 'COUPLE20') {
+        console.log('you get 50%')
+        couponSection.classList.add('hidden')
+    }
+    else{
+        alert('Please use a vaild coupon')
+    }
+})
 
